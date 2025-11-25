5597f5b791864adaa37224f754b226b49757d45c
a13kz <nils.alexander.bohre@gmail.com>
2025-11-25 09:55:22 +0100

    updating serial_coms

--- dataProcessing/serial_coms.py
+++ dataProcessing/serial_coms.py
@@ -3,23 +3,35 @@ import serial.tools.list_ports
 import datetime
 import os.path
 
+
 active_port = ""
 ports = serial.tools.list_ports.grep('USB')
 path = 'C:/Users/alexander.bohre/Documents/Arduino/gymnasie_arbete/collected_data/'
 today = datetime.date.today()
-current_time = datetime.datetime.now().strftime("%H:%M:%S")
+
+def assign_index(arr):
+    
+    return
 
 for p in ports:
     active_port = p.device
-    print(os.path.isfile(path+str(today)+'.txt'))
-    ser = serial.Serial(active_port, 115200)
+    break
+ser = serial.Serial(active_port, 115200)
+
+print("Location for measure?")
+location = input()
+
 while True:     
-    line=str(ser.readline())
-    arr = line[2:][:-5].split()
+    line=ser.readline().decode('latin1').strip()
+    print(line)
+    current_time = datetime.datetime.now().strftime("%H:%M:%S")
+    arr = line.split()
     print(arr)
-    with open(path+'/temperatures/'+str(today)+'temperature'+'.txt', "a") as f:
-        f.write(f'{arr[0]} {current_time} \n')
-    with open(path+'/pressures/'+str(today)+'pressure'+'.txt', "a") as f:
-        f.write(f'{arr[1]} {current_time} \n')
-    with open(path+'/altitudes/'+str(today)+'altitude'+'.txt', "a") as f:
-        f.write(f'{arr[2]} {current_time} \n')
+    if len(arr) == 3:
+        with open(f"{path}/temperatures/{str(today)}_{location}_temperature.txt", "a") as f:
+            f.write(f'{arr[0]} {current_time} \n')
+        with open(f"{path}/temperatures/{str(today)}_{location}_pressure.txt", "a") as f:
+            f.write(f'{arr[1]} {current_time} \n')
+        with open(f"{path}/temperatures/{str(today)}_{location}_altitude.txt", "a") as f:
+            f.write(f'{arr[2]} {current_time} \n')
+
